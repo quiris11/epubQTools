ca2434379183a940f9851b20fba3cfb03d12929d
Robert Błaut <listy@blaut.biz>
2014-05-03 21:12:06 +0200

    ability to run the app from zip file

---
+++ __main__.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of epubQTools, licensed under GNU Affero GPLv3 or later.
+# Copyright © Robert Błaut. See NOTICE for more information.
+#
+
+import argparse
+import sys
+import os
+if not hasattr(sys, 'frozen'):
+    sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
+from epubqcheck import qcheck
+from epubqfix import qfix
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("directory", help="Directory with EPUB files stored")
+parser.add_argument("-n", "--rename", help="rename .epub files to "
+                    "'author - title.epub'",
+                    action="store_true")
+parser.add_argument("-q", "--qcheck", help="validate files with epubqcheck "
+                    "internal tool",
+                    action="store_true")
+parser.add_argument("-p", "--epubcheck", help="validate epub files with "
+                    " EpubCheck 3.0.1 tool",
+                    action="store_true")
+parser.add_argument("-m", "--mod", help="validate only _moh.epub files "
+                    "(only with -q or -v)",
+                    action="store_true")
+parser.add_argument("-e", "--epub", help="fix and hyphenate original epub "
+                    "files to _moh.epub files", action="store_true")
+parser.add_argument("-r", "--resetmargins", help="reset CSS margins for "
+                    "body, html and @page in _moh.epub files (only with -e)",
+                    action="store_true")
+parser.add_argument("-c", "--findcover", help="force find cover (risky) "
+                    "(only with -e)",
+                    action="store_true")
+parser.add_argument("-t", "--replacefonts", help="replace font (experimental) "
+                    "(only with -e)",
+                    action="store_true")
+parser.add_argument("-k", "--kindlegen", help="convert _moh.epub files to"
+                    " .mobi with kindlegen", action="store_true")
+parser.add_argument("-d", "--huffdic", help="tell kindlegen to use huffdic "
+                    "compression (slow conversion)", action="store_true")
+parser.add_argument("-f", "--force",
+                    help="overwrite previously generated _moh.epub or "
+                    " .mobi files (only with -k or -e)",
+                    action="store_true")
+args = parser.parse_args()
+
+
+def main():
+    if args.qcheck or args.rename:
+        qcheck(args.directory, args.mod, args.epubcheck, args.rename)
+    elif args.kindlegen:
+        compression = '-c2' if args.huffdic else '-c1'
+        for root, dirs, files in os.walk(args.directory):
+            for _file in files:
+                if _file.endswith('_moh.epub'):
+                    newmobifile = os.path.splitext(_file)[0] + '.mobi'
+                    if not args.force:
+                        if os.path.isfile(os.path.join(root, newmobifile)):
+                            print(
+                                'Skipping previously generated _moh file: ' +
+                                newmobifile.decode(sys.getfilesystemencoding())
+                            )
+                            continue
+                    print('')
+                    print('Kindlegen: Converting file: ' +
+                          _file.decode(sys.getfilesystemencoding()))
+                    proc = subprocess.Popen([
+                        'kindlegen',
+                        '-dont_append_source',
+                        compression,
+                        os.path.join(root, _file)
+                    ], stdout=subprocess.PIPE).communicate()[0]
+
+                    cover_html_found = False
+                    for ln in proc.splitlines():
+                        if ln.find('Warning') != -1:
+                            print(ln)
+                        if ln.find('Error') != -1:
+                            print(ln)
+                        if ln.find('I1052') != -1:
+                            cover_html_found = True
+                    if not cover_html_found:
+                        print('')
+                        print(
+                            'WARNING: Probably duplicated covers '
+                            'generated in file: ' +
+                            newmobifile.decode(sys.getfilesystemencoding())
+                        )
+    elif args.epub:
+        qfix(args.directory, args.force, args.replacefonts, args.resetmargins,
+             args.findcover)
+    else:
+        parser.print_help()
+        print("* * *")
+        print("* At least one of above optional arguments is required.")
+        print("* * *")
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
--- lib/epubqfix.py
+++ lib/epubqfix.py
@@ -5,7 +5,6 @@
 # Copyright © Robert Błaut. See NOTICE for more information.
 #
 
-import argparse
 import hashlib
 import os
 import re
@@ -23,15 +22,13 @@ if not hasattr(sys, 'frozen'):
     sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
 from htmlconstants import entities
 from hyphenator import Hyphenator
-from epubqcheck import qcheck
 from os.path import expanduser
-
 
 MY_LANGUAGE = 'pl'
 HYPHEN_MARK = u'\u00AD'
 if not hasattr(sys, 'frozen'):
-    _hyph = Hyphenator(os.path.join(os.path.dirname(__file__), 'resources',
-                       'dictionaries', 'hyph_pl_PL.dic'))
+    _hyph = Hyphenator(os.path.join(os.path.dirname(__file__), '..', 
+                       'resources', 'dictionaries', 'hyph_pl_PL.dic'))
 else:
     _hyph = Hyphenator(os.path.join(os.path.dirname(sys.executable),
                        'resources', 'dictionaries', 'hyph_pl_PL.dic'))
@@ -47,45 +44,6 @@ SVGNS = {'svg': 'http://www.w3.org/2000/svg'}
 ADOBE_OBFUSCATION = 'http://ns.adobe.com/pdf/enc#RC'
 IDPF_OBFUSCATION = 'http://www.idpf.org/2008/embedding'
 CRNS = {'cr': 'urn:oasis:names:tc:opendocument:xmlns:container'}
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("directory", help="Directory with EPUB files stored")
-parser.add_argument("-n", "--rename", help="rename .epub files to "
-                    "'author - title.epub'",
-                    action="store_true")
-parser.add_argument("-q", "--qcheck", help="validate files with epubqcheck "
-                    "internal tool",
-                    action="store_true")
-parser.add_argument("-p", "--epubcheck", help="validate epub files with "
-                    " EpubCheck 3.0.1 tool",
-                    action="store_true")
-parser.add_argument("-m", "--mod", help="validate only _moh.epub files "
-                    "(only with -q or -v)",
-                    action="store_true")
-parser.add_argument("-e", "--epub", help="fix and hyphenate original epub "
-                    "files to _moh.epub files", action="store_true")
-parser.add_argument("-r", "--resetmargins", help="reset CSS margins for "
-                    "body, html and @page in _moh.epub files (only with -e)",
-                    action="store_true")
-parser.add_argument("-c", "--findcover", help="force find cover (risky) "
-                    "(only with -e)",
-                    action="store_true")
-parser.add_argument("-t", "--replacefonts", help="replace font (experimental) "
-                    "(only with -e)",
-                    action="store_true")
-parser.add_argument("-k", "--kindlegen", help="convert _moh.epub files to"
-                    " .mobi with kindlegen", action="store_true")
-parser.add_argument("-d", "--huffdic", help="tell kindlegen to use huffdic "
-                    "compression (slow conversion)", action="store_true")
-parser.add_argument("-f", "--force",
-                    help="overwrite previously generated _moh.epub or "
-                    " .mobi files (only with -k or -e)",
-                    action="store_true")
-args = parser.parse_args()
-
-_documents = args.directory
-validator = args.epubcheck
 
 
 def check_font(path):
@@ -377,7 +335,8 @@ def fix_html_toc(soup, tempdir, xhtml_files, xhtml_file_paths):
             parser = etree.XMLParser(remove_blank_text=True)
             if not hasattr(sys, 'frozen'):
                 transform = etree.XSLT(etree.parse(os.path.join(
-                    os.path.dirname(__file__), 'resources', 'ncx2end-0.2.xsl'
+                    os.path.dirname(__file__), '..',
+                    'resources', 'ncx2end-0.2.xsl'
                 )))
             else:
                 transform = etree.XSLT(etree.parse(os.path.join(
@@ -664,7 +623,7 @@ def fix_various_opf_problems(soup, tempdir, xhtml_files,
                     metacovers, soup, itemhrefcovers[0].get('id')
                 )
 
-    elif len(metacovers) == 0 and len(refcovers) == 0 and args.findcover:
+    elif len(metacovers) == 0 and len(refcovers) == 0 and _findcover:
         imag_href, imag_id = force_cover_find(soup)
         if imag_href is not None and imag_id is not None:
             soup = set_cover_guide_ref(
@@ -820,7 +779,7 @@ def replace_svg_html_cover(opftree, rootepubdir):
     if len(svg_imgs) == 1:
         if not hasattr(sys, 'frozen'):
             new_cover_tree = etree.parse(os.path.join(
-                os.path.dirname(__file__), 'resources', 'cover.xhtml'
+                os.path.dirname(__file__), '..', 'resources', 'cover.xhtml'
             ))
         else:
             new_cover_tree = etree.parse(os.path.join(
@@ -891,200 +850,149 @@ def remove_file_from_epub(file_rel_to_opf, opftree, rootepubdir):
     os.remove(os.path.join(rootepubdir, file_rel_to_opf))
 
 
-def main():
-    if args.qcheck or args.rename:
-        qcheck(_documents, args.mod, args.epubcheck, args.rename)
-    elif args.kindlegen:
-        compression = '-c2' if args.huffdic else '-c1'
-        for root, dirs, files in os.walk(_documents):
-            for _file in files:
-                if _file.endswith('_moh.epub'):
-                    newmobifile = os.path.splitext(_file)[0] + '.mobi'
-                    if not args.force:
-                        if os.path.isfile(os.path.join(root, newmobifile)):
-                            print(
-                                'Skipping previously generated _moh file: ' +
-                                newmobifile.decode(sys.getfilesystemencoding())
-                            )
-                            continue
-                    print('')
-                    print('Kindlegen: Converting file: ' +
-                          _file.decode(sys.getfilesystemencoding()))
-                    proc = subprocess.Popen([
-                        'kindlegen',
-                        '-dont_append_source',
-                        compression,
-                        os.path.join(root, _file)
-                    ], stdout=subprocess.PIPE).communicate()[0]
+def qfix(_documents, _forced, _replacefonts, _resetmargins, _findcover):
+    for root, dirs, files in os.walk(_documents):
+        for _file in files:
+            if (_file.endswith('.epub') and
+                    not _file.endswith('_moh.epub') and
+                    not _file.endswith('_org.epub')):
+                _newfile = os.path.splitext(_file)[0] + '_moh.epub'
 
-                    cover_html_found = False
-                    for ln in proc.splitlines():
-                        if ln.find('Warning') != -1:
-                            print(ln)
-                        if ln.find('Error') != -1:
-                            print(ln)
-                        if ln.find('I1052') != -1:
-                            cover_html_found = True
-                    if not cover_html_found:
-                        print('')
+                # if not forced skip previously generated files
+                if not _forced:
+                    if os.path.isfile(os.path.join(root, _newfile)):
                         print(
-                            'WARNING: Probably duplicated covers '
-                            'generated in file: ' +
-                            newmobifile.decode(sys.getfilesystemencoding())
+                            'Skipping previously generated _moh file: ' +
+                            _newfile.decode(sys.getfilesystemencoding())
+                        )
+                        continue
+
+                print('')
+                print('Working on: ' +
+                      _file.decode(sys.getfilesystemencoding()))
+                _epubzipfile, _tempdir = unpack_epub(
+                    os.path.join(root, _file)
+                )
+
+                opf_dir, opf_file_path = find_roots(_tempdir)
+                opf_dir_abs = os.path.join(_tempdir, opf_dir)
+                opf_file_path_abs = os.path.join(_tempdir, opf_file_path)
+
+                # remove obsolete calibre_bookmarks.txt
+                try:
+                    os.remove(os.path.join(
+                        _tempdir, 'META-INF', 'calibre_bookmarks.txt'
+                    ))
+                except OSError:
+                    pass
+
+                (
+                    opftree, _xhtml_files, _xhtml_file_paths
+                ) = find_xhtml_files(
+                    _epubzipfile, _tempdir, opf_dir_abs, opf_file_path_abs
+                )
+
+                opftree = fix_various_opf_problems(
+                    opftree, opf_dir_abs,
+                    _xhtml_files, _xhtml_file_paths
+                )
+
+                opftree = fix_ncx_dtd_uid(opftree, opf_dir_abs)
+
+                opftree = fix_html_toc(
+                    opftree, opf_dir_abs,
+                    _xhtml_files, _xhtml_file_paths
+                )
+
+                opftree = fix_meta_cover_order(opftree)
+
+                # experimental - disabled
+                # replace_svg_html_cover(opftree, opf_dir_abs)
+
+                fix_mismatched_covers(opftree, opf_dir_abs)
+
+                convert_dl_to_ul(opftree, opf_dir_abs)
+                remove_text_from_html_cover(opftree, opf_dir_abs)
+
+                # parse encryption.xml file
+                enc_file = os.path.join(
+                    _tempdir, 'META-INF', 'encryption.xml'
+                )
+                if os.path.exists(enc_file):
+                    process_encryption(enc_file, opftree)
+                    os.remove(enc_file)
+
+                if _replacefonts:
+                    find_and_replace_fonts(opftree, opf_dir_abs)
+
+                hyph_info_printed = res_css_info_printed = False
+                for _single_xhtml in _xhtml_files:
+                    with open(_single_xhtml, 'r') as content_file:
+                        c = content_file.read()
+                    for key in entities.iterkeys():
+                        c = c.replace(key, entities[key])
+                    _xhtmltree = etree.fromstring(
+                        c, parser=etree.XMLParser(recover=False)
+                    )
+
+                    if opftree.xpath(
+                            "//dc:language", namespaces=DCNS
+                    )[0].text == 'pl':
+                        if not hyph_info_printed:
+                            print('Hyphenating book with Polish '
+                                  'dictionary...')
+                            hyph_info_printed = True
+                        _xhtmltree = hyphenate_and_fix_conjunctions(
+                            _xhtmltree, _hyph, HYPHEN_MARK
                         )
 
-    elif args.epub:
-        for root, dirs, files in os.walk(_documents):
-            for _file in files:
-                if (_file.endswith('.epub') and
-                        not _file.endswith('_moh.epub') and
-                        not _file.endswith('_org.epub')):
-                    _newfile = os.path.splitext(_file)[0] + '_moh.epub'
+                    _xhtmltree = fix_styles(_xhtmltree)
 
-                    # if not forced skip previously generated files
-                    if not args.force:
-                        if os.path.isfile(os.path.join(root, _newfile)):
-                            print(
-                                'Skipping previously generated _moh file: ' +
-                                _newfile.decode(sys.getfilesystemencoding())
-                            )
-                            continue
+                    if _resetmargins:
+                        if not res_css_info_printed:
+                            print('Resetting CSS body margin and padding'
+                                  '...')
+                        res_css_info_printed = True
+                        _xhtmltree = append_reset_css(_xhtmltree)
 
-                    print('')
-                    print('Working on: ' +
-                          _file.decode(sys.getfilesystemencoding()))
-                    _epubzipfile, _tempdir = unpack_epub(
-                        os.path.join(root, _file)
-                    )
+                    _xhtmltree = modify_problematic_styles(_xhtmltree)
 
-                    opf_dir, opf_file_path = find_roots(_tempdir)
-                    opf_dir_abs = os.path.join(_tempdir, opf_dir)
-                    opf_file_path_abs = os.path.join(_tempdir, opf_file_path)
+                    # remove watermarks
+                    _wmarks = etree.XPath(
+                        '//xhtml:span[starts-with(text(), "==")]',
+                        namespaces=XHTMLNS
+                    )(_xhtmltree)
+                    for wm in _wmarks:
+                        remove_node(wm)
 
-                    # remove obsolete calibre_bookmarks.txt
-                    try:
-                        os.remove(os.path.join(
-                            _tempdir, 'META-INF', 'calibre_bookmarks.txt'
-                        ))
-                    except OSError:
-                        pass
+                    # remove meta charsets
+                    _metacharsets = etree.XPath(
+                        '//xhtml:meta[@charset="utf-8"]',
+                        namespaces=XHTMLNS
+                    )(_xhtmltree)
+                    for mch in _metacharsets:
+                        mch.getparent().remove(mch)
 
-                    (
-                        opftree, _xhtml_files, _xhtml_file_paths
-                    ) = find_xhtml_files(
-                        _epubzipfile, _tempdir, opf_dir_abs, opf_file_path_abs
-                    )
-
-                    opftree = fix_various_opf_problems(
-                        opftree, opf_dir_abs,
-                        _xhtml_files, _xhtml_file_paths
-                    )
-
-                    opftree = fix_ncx_dtd_uid(opftree, opf_dir_abs)
-
-                    opftree = fix_html_toc(
-                        opftree, opf_dir_abs,
-                        _xhtml_files, _xhtml_file_paths
-                    )
-
-                    opftree = fix_meta_cover_order(opftree)
-
-                    # experimental - disabled
-                    # replace_svg_html_cover(opftree, opf_dir_abs)
-
-                    fix_mismatched_covers(opftree, opf_dir_abs)
-
-                    convert_dl_to_ul(opftree, opf_dir_abs)
-                    remove_text_from_html_cover(opftree, opf_dir_abs)
-
-                    # parse encryption.xml file
-                    enc_file = os.path.join(
-                        _tempdir, 'META-INF', 'encryption.xml'
-                    )
-                    if os.path.exists(enc_file):
-                        process_encryption(enc_file, opftree)
-                        os.remove(enc_file)
-
-                    if args.replacefonts:
-                        find_and_replace_fonts(opftree, opf_dir_abs)
-
-                    hyph_info_printed = res_css_info_printed = False
-                    for _single_xhtml in _xhtml_files:
-                        with open(_single_xhtml, 'r') as content_file:
-                            c = content_file.read()
-                        for key in entities.iterkeys():
-                            c = c.replace(key, entities[key])
-                        _xhtmltree = etree.fromstring(
-                            c, parser=etree.XMLParser(recover=False)
-                        )
-
-                        if opftree.xpath(
-                                "//dc:language", namespaces=DCNS
-                        )[0].text == 'pl':
-                            if not hyph_info_printed:
-                                print('Hyphenating book with Polish '
-                                      'dictionary...')
-                                hyph_info_printed = True
-                            _xhtmltree = hyphenate_and_fix_conjunctions(
-                                _xhtmltree, _hyph, HYPHEN_MARK
-                            )
-
-                        _xhtmltree = fix_styles(_xhtmltree)
-
-                        if args.resetmargins:
-                            if not res_css_info_printed:
-                                print('Resetting CSS body margin and padding'
-                                      '...')
-                            res_css_info_printed = True
-                            _xhtmltree = append_reset_css(_xhtmltree)
-
-                        _xhtmltree = modify_problematic_styles(_xhtmltree)
-
-                        # remove watermarks
-                        _wmarks = etree.XPath(
-                            '//xhtml:span[starts-with(text(), "==")]',
-                            namespaces=XHTMLNS
-                        )(_xhtmltree)
-                        for wm in _wmarks:
-                            remove_node(wm)
-
-                        # remove meta charsets
-                        _metacharsets = etree.XPath(
-                            '//xhtml:meta[@charset="utf-8"]',
-                            namespaces=XHTMLNS
-                        )(_xhtmltree)
-                        for mch in _metacharsets:
-                            mch.getparent().remove(mch)
-
-                        with open(_single_xhtml, "w") as f:
-                            f.write(etree.tostring(
-                                _xhtmltree,
-                                pretty_print=True,
-                                xml_declaration=True,
-                                encoding="utf-8",
-                                doctype=DTD)
-                            )
-                    opftree = remove_wm_info(opftree, opf_dir_abs)
-
-                    # write all OPF changes back to file
-                    with open(opf_file_path_abs, 'w') as f:
+                    with open(_single_xhtml, "w") as f:
                         f.write(etree.tostring(
-                            opftree.getroot(),
+                            _xhtmltree,
                             pretty_print=True,
                             xml_declaration=True,
-                            encoding='utf-8'
-                        ))
+                            encoding="utf-8",
+                            doctype=DTD)
+                        )
+                opftree = remove_wm_info(opftree, opf_dir_abs)
 
-                    pack_epub(os.path.join(root, _newfile),
-                              _tempdir)
-                    clean_temp(_tempdir)
-                    print('Done...')
-    else:
-        parser.print_help()
-        print("* * *")
-        print("* At least one of above optional arguments is required.")
-        print("* * *")
-    return 0
+                # write all OPF changes back to file
+                with open(opf_file_path_abs, 'w') as f:
+                    f.write(etree.tostring(
+                        opftree.getroot(),
+                        pretty_print=True,
+                        xml_declaration=True,
+                        encoding='utf-8'
+                    ))
 
-if __name__ == '__main__':
-    sys.exit(main())
+                pack_epub(os.path.join(root, _newfile),
+                          _tempdir)
+                clean_temp(_tempdir)
+                print('Done...')
