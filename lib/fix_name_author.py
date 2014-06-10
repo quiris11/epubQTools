772b2b54b19227749ccd68e7240b79fccd72ae5d
Robert Błaut <listy@blaut.biz>
2014-06-10 09:26:48 +0200

    initial work for update title and author tool - not ready yet

---
+++ lib/fix_name_author.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of epubQTools, licensed under GNU Affero GPLv3 or later.
+# Copyright © Robert Błaut. See NOTICE for more information.
+#
+
+from __future__ import print_function
+import zipfile
+import os
+import sys
+from lxml import etree
+from lib.epubqfix import pack_epub
+from lib.epubqfix import unpack_epub
+from lib.epubqfix import clean_temp
+from lib.epubqfix import find_roots
+
+SFENC = sys.getfilesystemencoding()
+OPFNS = {'opf': 'http://www.idpf.org/2007/opf'}
+DCNS = {'dc': 'http://purl.org/dc/elements/1.1/'}
+
+
+def set_author(tree):
+    crs = tree.xpath('//dc:creator/', namespaces=DCNS)
+    for c in crs:
+        print(c)
+
+
+def set_title(tree):
+    ts = tree.xpath('//dc:title/', namespaces=DCNS)
+    for t in ts:
+        print(t)
+
+
+def fix_name_author(root, f, name, author):
+    print('')
+    print('START qfix for: ' + f.decode(SFENC))
+    try:
+        tempdir = unpack_epub(os.path.join(root, f))
+    except zipfile.BadZipfile:
+        print('Unable to process corrupted file...')
+        return 0
+    opfd, opff = find_roots(tempdir)
+    opff_abs = os.path.join(tempdir, opff)
+    parser = etree.XMLParser(remove_blank_text=True)
+    opftree = etree.parse(opff_abs, parser)
+    set_author(opftree)
+    set_title(opftree)
+    newfile = f.splitext(0) + '_m.epub'
+    pack_epub(os.path.join(root, newfile), tempdir)
+    clean_temp(tempdir)
+    print('FINISH qfix for: ' + f.decode(SFENC))
