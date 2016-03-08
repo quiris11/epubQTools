c142f20a6a7f4fd20f22f7c3a3ea02f29f71c465
Robert Błaut <listy@blaut.biz>
2016-03-08 15:08:57 +0100

    ...

--- lib/parsecss.py
+++ lib/parsecss.py
@@ -7,24 +7,31 @@
 
 from __future__ import print_function
 import os
-import tinycss
+import cssutils
 
-parser = tinycss.make_parser()
+
+def rename_font_family(old_name, new_name, sheet):
+    for r in sheet:
+        for p in r.style:
+            if p.name == 'font-family' and (old_name in p.value):
+                print('1', p.name, p.value)
+                p.value = p.value.replace(old_name, new_name)
+                print('2', p.name, p.value)
+
+
 with open(os.path.join(os.path.expanduser('~'), 'style.css')) as f:
     content = f.read()
-# print(content)
-ss = parser.parse_stylesheet(content)
-# print(ss.errors)
-for r in ss.rules:
-    # print(r.selector)
-    if r.selector is not None:
-        # for s in r.selector:
-        # if s.value == 'body':
-        for d in r.declarations:
-            if d.name == 'font-family':
-                # for t in d.value:
-                    # if t.type == 'STRING' or t.type == 'IDENT':
-                        print(d.priority,
-                              r.selector.as_css(),
-                              '###',
-                              d.value.as_css())
+sheet = cssutils.parseString(content, validate=True)
+rename_font_family('Alegreya', 'test', sheet)
+for rule in sheet:
+    if rule.type == rule.FONT_FACE_RULE:
+        for p in rule.style:
+            if p.name == 'font-family':
+                fname = p.value
+            if p.name == 'font-weight':
+                fbold = p.value
+            if p.name == 'font-style':
+                fitalic = p.value
+            if p.name == 'src':
+                furl = p.value
+        print(fname, fbold, fitalic, furl)
